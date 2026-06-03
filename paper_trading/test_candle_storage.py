191eae58f67ac8aeba8e4762706b8f63d589c063
spark-03 <123577615+spark-03@users.noreply.github.com>
2026-06-03 15:00:16 +0530

    Update test_candle_storage.py

--- paper_trading/test_candle_storage.py
+++ paper_trading/test_candle_storage.py
@@ -1,1 +1,25 @@
+import pandas as pd
 
+from paper_trading.candle_storage import CandleStorage
+
+df = pd.DataFrame(
+    [
+        {
+            "datetime": "2026-06-03 09:15:00",
+            "open": 100,
+            "high": 110,
+            "low": 95,
+            "close": 108,
+            "volume": 10000,
+        }
+    ]
+)
+
+storage = CandleStorage()
+
+storage.save_symbol_candles(
+    "RELIANCE-EQ",
+    df,
+)
+
+print("done")
